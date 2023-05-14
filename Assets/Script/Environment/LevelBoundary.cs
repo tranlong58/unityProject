6ab8749404186bc83f0f6e0729baa0ced8297679
unknown <onglongpro01@gmail.com>
2023-05-14 17:50:12 +0700

    init project

---
+++ Assets/Script/Environment/LevelBoundary.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelBoundary : MonoBehaviour
+{
+    public static float leftSide = -3.5f, rightSide = 3.5f;
+    public float internalLeft, internalRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        internalLeft = leftSide;
+        internalRight = rightSide;
+    }
+}
